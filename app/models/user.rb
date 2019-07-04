538ee37d4169b8a9491feaa7aba5272fb93aa59b
Bodia3151 <bodia.savryha@gmail.com>
2019-07-04 17:53:47 +0300

    Add validations for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
-  enum role: [ :customer, :manager , :admin ]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  LENGTHEMAIL = (6..50).to_a.freeze
+  ROLE = %i[customer manager admin].freeze
+  enum role: [ROLE]
+
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.a[a-z]+)*\.[a-z]+\z/i }, uniqueness: true
+  validates_length_of :email, in: LENGTHEMAIL
+  validates :role, presence: true
+  validates :role, inclusion: { in: ROLE }
 end
