085e3740eb4aa1e8b89fdd5fc072271a834ce00b
Chamelek <stetsyshyn.vo@gmail.com>
2019-07-04 22:36:47 +0300

    add nil parameter and default to user in schema

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,7 +1,2 @@
 class City < ApplicationRecord
-  MAX_NAME_LENGTH = 50
-  has_many :districts
-  has_many :users
-
-  validates :name, presence: true
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,13 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  describe 'Associations' do
-    xit { should have_many(:districts) }
-    xit { should have_many(:users) }
-  end
-
-  describe 'Validate' do
-    xit { should validate_presence_of(:name) }
-    xit { should validate_length_of(:name).is_at_most(MAX_NAME_LENGTH) }
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
