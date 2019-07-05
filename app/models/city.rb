51fc37fe9d6ec5617135ddba8e2246d175004b72
Chamelek <stetsyshyn.vo@gmail.com>
2019-07-05 18:30:10 +0300

    add uniqueness validation to model city

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,9 @@
 class City < ApplicationRecord
   MAX_NAME_LENGTH = 30
   MIN_NAME_LENGTH = 3
-  validates :name, presence: true, length: { minimum: MIN_NAME_LENGTH, maximum: MAX_NAME_LENGTH }
+  
+  validates :name, presence: true
+  validates :name, length: { minimum: MIN_NAME_LENGTH, maximum: MAX_NAME_LENGTH }
   validates :name, format: { with: /\A[\'\ а-щА-ЩЬьЮюЯяЇїІіЄєҐґ-]+\z/ }
+  validates :name, uniqueness: true
 end
