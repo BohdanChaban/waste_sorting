2e5fdf4c6a973042adbe22805c1448acaef6f5df
Chamelek <stetsyshyn.vo@gmail.com>
2019-07-05 23:31:26 +0300

    delete edit view and add index to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :cities, only: %i[create new show]
+  resources :cities, only: %i[index create new show]
 
   root to: 'cities#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
