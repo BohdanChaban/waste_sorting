a4ccabdbdf532a1a323c66853e70dcefb097be29
Chamelek <stetsyshyn.vo@gmail.com>
2019-07-05 17:30:28 +0300

    delete some comments

--- spec/controllers/cities_controller_spec.rb
+++ spec/controllers/cities_controller_spec.rb
@@ -27,9 +27,7 @@ RSpec.describe CitiesController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # City. As you add validations to City, be sure to
   # adjust the attributes here as well.
-  # let(:valid_attributes) { { name: 'Lviv' } }
 
-  # let(:invalid_attributes) { { name: nil } }
   let(:city) { FactoryBot.create(:valid_city) }
 
   # This should return the minimal set of values that should be in the session
@@ -39,7 +37,6 @@ RSpec.describe CitiesController, type: :controller do
 
   describe 'GET #index' do
     it 'returns a success response' do
-      # City.create! valid_attributes
       get :index, params: {}, session: valid_session
       expect(response).to be_successful
     end
@@ -47,7 +44,6 @@ RSpec.describe CitiesController, type: :controller do
 
   describe 'GET #show' do
     it 'returns a success response' do
-      # city = City.create! valid_attributes
       get :show, params: { id: city.to_param }, session: valid_session
       expect(response).to be_successful
     end
