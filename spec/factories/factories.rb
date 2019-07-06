47e87584a489b95d727710d73d38a723939a8e65
Bodia3151 <bodia.savryha@gmail.com>
2019-07-06 14:23:20 +0300

    test for user model

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -3,5 +3,20 @@ FactoryBot.define do
     email { 'vasia12@gmail.com' }
     role { 'customer' }
     password { '12345678' }
+
+    factory :user_customer do
+      email { 'customer@gmail.com' }
+      role { 'customer' }
+    end
+
+    factory :user_manager do
+      email { 'manager@gmail.com' }
+      role { 'manager' }
+    end
+
+    factory :user_admin do
+      email { 'admin@gmail.com' }
+      role { 'admin' }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,47 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'check validation of user ' do
-    let(:user) { build(:user) }
+  let(:short_email) { 'email' }
+  let(:long_email)  { short_email * 30 }
+  let(:default_role) { 'customer' }
+  let(:inclusion_role) { %w[customer manager admin] }
+  let(:not_inclusion_role) { 'vasya' }
+  let(:valid_email) { 'bodia@gmail.com' }
 
-    it 'return invalid user' do
+  context 'check validation of user ' do
+    it 'is valid with valid attributes' do
+      user = FactoryBot.create(:user)
       expect(user).to be_valid
+    end
+
+    it 'is not valid with invalid attributes' do
+      user = FactoryBot.build(:user, email: nil, role: nil, password: nil)
+      expect(user).not_to be_valid
     end
   end
 
   context 'check validation of email ' do
-    let(:user) { build(:user, email: 'bodia31@gmail.com') }
-    let(:user_invalid_empty) { build(:user, email: '') }
-    let(:user_invalid_short) { build(:user, email: 'qwe') }
-    let(:user_invalid_format) { build(:user, email: '@vasiaa') }
-
-    it 'return invalid email' do
+    it 'is valid with email attributes' do
+      user = FactoryBot.build(:user, email: valid_email)
       expect(user).to be_valid
     end
 
-    it 'return invalid empty' do
-      expect(user_invalid_empty).to be_invalid
+    it 'is not valid with short email' do
+      user = FactoryBot.build(:user, email: short_email)
+      expect(user).to be_invalid
     end
 
-    it 'return invalid short' do
-      expect(user_invalid_short).to be_invalid
+    it 'is not valid with long email' do
+      user = FactoryBot.build(:user, email: long_email)
+      expect(user).to be_invalid
     end
 
-    it 'return invalid format' do
-      expect(user_invalid_format).to be_invalid
+    it 'is valid with valid attributes' do
+      user = FactoryBot.create(:user)
+      expect(user.role).to eq default_role
+    end
+
+    it 'is allow inclusion value in the role' do
+      user = FactoryBot.create(:user)
+      inclusion_role.each do |role|
+        user.role = role
+        expect(user).to be_valid
+      end
+    end
+    it 'is not allow not inclusion value in the role' do
+      user = FactoryBot.create(:user)
+      expect { user.role = not_inclusion_role }.to raise_error(ArgumentError)
     end
   end
 
-  context 'check validation of role' do
-    let(:user) { build(:user, role: 'admin') }
-    let(:user_invalid_role) { build(:user, role: 'vasya') }
-
-    it 'return invalid user' do
-      expect(user).to be_valid
-    end
-
-    it 'return invalid role' do
-      expect { build(user_invalid_role) }.to raise_error(ArgumentError)
+  context 'check already exist of email' do
+    it 'should validate uniqueness of email' do
+      user1 = FactoryBot.create(:user, email: valid_email)
+      user1.save
+      user2 = FactoryBot.build(:user, email: valid_email)
+      expect { user2.save! }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 end
