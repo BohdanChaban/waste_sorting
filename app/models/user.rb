192c1cf10f48cf2df5398c496d4e5f7851232212
Bodia3151 <48513569+Bodia3151@users.noreply.github.com>
2019-07-07 13:54:23 +0300

    Add Devise and User model (#7)

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  EMAIL_LENGTH = (6..50).freeze
+  ROLES = %w[customer manager admin].freeze
+  enum role: ROLES
+
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.a[a-z]+)*\.[a-z]+\z/i }, uniqueness: true
+  validates_length_of :email, in: EMAIL_LENGTH
+  validates :role, presence: true, inclusion: { in: ROLES }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :cities, only: %i[index create new show]
-
   root to: 'cities#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
