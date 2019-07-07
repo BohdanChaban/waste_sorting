1f990ed0ea510ecf5637fa70c1629f46e56a2e4a
pashko37 <evobbs5@gmail.com>
2019-07-07 22:36:38 +0300

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :articles
   devise_for :users
   resources :cities, only: %i[index create new show]
-  root to: 'cities#index'
+  root to: 'articles#index'
 end
