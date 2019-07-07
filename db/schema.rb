7b69247f17eabed0c849c5ee468fecaeca7d7f4e
pashko37 <34835861+pashko37@users.noreply.github.com>
2019-07-07 14:58:18 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,11 @@ ActiveRecord::Schema.define(version: 2019_07_04_150029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
+  create_table "cities", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
