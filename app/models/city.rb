42421f39d2a1a528ce657bf16269434543c1625d
pashko37 <evobbs5@gmail.com>
2019-07-07 13:48:09 +0300

    fix

--- app/models/city.rb
+++
@@ -1,9 +0,0 @@
-class City < ApplicationRecord
-  MAX_NAME_LENGTH = 30
-  MIN_NAME_LENGTH = 3
-
-  validates :name, presence: true
-  validates :name, length: { minimum: MIN_NAME_LENGTH, maximum: MAX_NAME_LENGTH }
-  validates :name, format: { with: /\A[\'\ а-щА-ЩЬьЮюЯяЇїІіЄєҐґ-]+\z/ }
-  validates :name, uniqueness: true
-end
--- spec/factories/city_factory.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :city do
-    factory :valid_city do
-      name { 'Львів' }
-    end
-  end
-end
