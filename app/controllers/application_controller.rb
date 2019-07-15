d3bd49bfcba6ba1c63e1dde751990f4cbb59bccc
Bodia3151 <48513569+Bodia3151@users.noreply.github.com>
2019-07-15 21:18:59 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(_resource)
     current_user.account ? root_path : new_account_path
-
+  end
+  
   private
 
   def check_admin_access
     redirect_to root_path unless current_user&.admin?
-r
   end
 end
