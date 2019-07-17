65acc0d3e8414656103eff1cd7b0c9b33bd1716f
pashko37 <34835861+pashko37@users.noreply.github.com>
2019-07-17 14:06:24 +0300

    Restrict access to CUD actions of articles (#20)
    * Restrict access to CUD actions of articles
    
    * fixed test

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   TITLE_LENGTH_RANGE = (5..40).freeze
+
   validates_length_of :title, in: TITLE_LENGTH_RANGE
   validates :title, presence: true
   validates :summary, presence: true
