087369731eeea4cbf06d4e76bd2bdbd85baa8bf1
Chamelek <stetsyshyn.vo@gmail.com>
2019-07-19 19:02:05 +0300

    add spec for contact helper

---
+++ spec/helpers/contacts_helper_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe ContactsHelper, type: :helper do
+  describe '#show_non_blank_field' do
+    let!(:contact) { FactoryBot.create(:valid_attributes) }
+
+    let(:label) do
+      { address: 'Address',
+        fb: 'Facebook' }
+    end
+
+    let(:non_link_field) { "<tr><th>#{label[:address]}:</th><td>#{contact.address}</td></tr>" }
+    let(:link_field) { "<tr><th>#{label[:fb]}:</th><td><a href=#{contact.facebook}>#{contact.facebook}</a></td></tr>" }
+    context 'shows non_blank_field' do
+      it 'without hyperlinks' do
+        expect(helper.show_non_blank_field(label[:address], contact.address)).to eql non_link_field
+      end
+      it 'with hyperlink' do
+        expect(helper.show_non_blank_field(label[:fb], contact.facebook)).to eql link_field
+      end
+    end
+  end
+end
