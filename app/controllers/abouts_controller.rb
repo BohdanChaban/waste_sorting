a33fdd24f862117820d4520b9616d3e046973931
Bodia3151 <48513569+Bodia3151@users.noreply.github.com>
2019-07-19 10:08:12 +0300

    add model about us (#21)

---
+++ app/controllers/abouts_controller.rb
@@ -0,0 +1,33 @@
+class AboutsController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :check_admin_access, only: %i[edit update]
+  before_action :set_about, only: %i[edit update]
+
+  def index
+    @about = About.last
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @about.update(about_params)
+        format.html { redirect_to abouts_path, notice: 'About was successfully updated.' }
+        format.json { render :show, status: :ok, location: @about }
+      else
+        format.html { render :edit }
+        format.json { render json: @about.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_about
+    @about = About.find(params[:id])
+  end
+
+  def about_params
+    params.require(:about).permit(:content)
+  end
+end
---
+++ spec/models/about_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe About, type: :model do
+  context 'check validation of about ' do
+    it 'is valid with valid attributes' do
+      about = FactoryBot.create(:about)
+      expect(about).to be_valid
+    end
+    it 'is invalid with invalid attributes' do
+      about = FactoryBot.build(:about_invalid)
+      expect(about).to_not be_valid
+    end
+  end
+end
