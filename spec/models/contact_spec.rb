1074a4221b9747a42b4e5447086c665976f53d19
Chamelek <stetsyshyn.vo@gmail.com>
2019-07-19 15:31:01 +0300

    add contact model spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'check validation of contact' do
+    it 'is valid with valid attributes' do
+      contact = FactoryBot.create(:valid_attributes)
+      expect(contact).to be_valid
+    end
+
+    it 'is not valid with invalid attributes' do
+      contact = FactoryBot.build(:invalid_attributes)
+      expect(contact).not_to be_valid
+    end
+  end
 end
