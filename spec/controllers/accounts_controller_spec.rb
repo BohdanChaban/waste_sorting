fac325fd5a0ff4c678817c51a38f4237fa501d8f
Chamelek <stetsyshyn.vo@gmail.com>
2019-07-29 13:21:13 +0300

    add 2 specs for create action account

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe AccountsController, type: :controller do
     { name: 'Name',
       surname: 'Surname',
       address: 'Шевченка, 77',
-      mobile_number: '+380998765952',
-      district_id: account.district_id }
+      mobile_number: '+380998765952' }
   end
 
   let(:invalid_account) do
@@ -74,26 +73,34 @@ RSpec.describe AccountsController, type: :controller do
   end
 
   describe 'POST #create', tdd1: true do
-    xcontext 'create account with valid params' do
-      # let(:user_customer) { FactoryBot.create(:user_customer) }
-      # let(:account) { nil }
+    let(:user_customer) { FactoryBot.create(:user_customer) }
+    let(:valid_account_with_district) do
+      { name: 'TestName',
+        surname: 'Surname',
+        address: 'Шевченка, 77',
+        mobile_number: '+380998765952',
+        district_id: district.id }
+    end
+
+    let(:district) { FactoryBot.create(:district_valid_name) }
+    context 'create account with valid params' do
       it 'when account not exist' do
-        login_with user
+        login_with user_customer
         expect do
-          post :create, params: { account: valid_account }, session: valid_session
+          post :create, params: { account: valid_account_with_district }, session: valid_session
         end.to change(Account, :count).by(1)
       end
 
       it 'redirects to the created account' do
-        login_with user
-        post :create, params: { account: valid_account }, session: valid_session
-        expect(response).to redirect_to(root_path)
+        login_with user_customer
+        post :create, params: { account: valid_account_with_district }, session: valid_session
+        expect(response).to redirect_to(account_path(user_customer.account))
       end
     end
 
     context 'with invalid params' do
       it "returns a success response (i.e. to display the 'new' template)" do
-        login_with user
+        login_with user_customer
         post :create, params: { account: invalid_account }, session: valid_session
         expect(response).to be_successful
       end
