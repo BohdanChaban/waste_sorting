b76c4c89e78f66a92029eddf7c9d5e156e730986
pashko37 <evobbs5@gmail.com>
2019-07-31 00:15:08 +0300

    add validation only one inquiry

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -6,9 +6,18 @@ class Inquiry < ApplicationRecord
   enum status: STATUS
 
   validates :status, presence: true, inclusion: { in: STATUS }
-  validate :free_timeslot
+  validate :free_timeslot, on: :create
+  validate :free_inquiry
 
   def free_timeslot
     errors.add(:timeslot, 'For this timeslot, all inquiries are busy') if timeslot.max_count_inquiry?
+  end
+
+  def free_inquiry
+    errors.add(:timeslot, 'You have already created queries in this timeslot.') if one_inquiry
+  end
+
+  def one_inquiry
+    Inquiry.find_by(user: user, timeslot_id: timeslot).present?
   end
 end
--- app/services/timeslot_service.rb
+++ app/services/timeslot_service.rb
@@ -9,7 +9,16 @@ class TimeslotService
     elsif @user&.manager?
       @user.timeslots
     elsif @user&.customer?
-      Timeslot.select { |t| t.district == @user.account.district }.reject(&:max_count_inquiry?)
+      customer_timeslot_view
+    end
+  end
+
+  def customer_timeslot_view
+    timeslot = Timeslot.select { |t| t.district == @user.account.district }.reject(&:max_count_inquiry?)
+    if timeslot.present?
+      timeslot
+    else
+      Timeslot.all
     end
   end
 end
