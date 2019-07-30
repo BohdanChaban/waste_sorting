cb3c1f93b9999b68f762fe1a3658bebd3c017ff2
Bodia3151 <bodia.savryha@gmail.com>
2019-07-30 12:20:51 +0300

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,14 @@ Contact.create(name: 'Head Office Waste Sorting LTD',
                instagram: 'https://instagram.com/waste_sorting',
                other_info: 'Zip Code: 79000')
 
-About.create(content: 'HELLOYYYYYYYYYYYYYYY')
+About.create(content: 'Waste Sorting a company that makes the world cleaner')
 
-districs_of_lviv = %w[
+district_names = %w[
   Шевченківський Личаківський Сихівський Франківський Залізничний Галицький
 ]
 
-City.create(name: 'Львів')
+city = City.create(name: 'Львів')
 
-districs_of_lviv.each do |name|
-  District.create(name: name, city_id: 1)
+district_names.each do |name|
+  District.create(name: name, city_id: city.id)
 end
