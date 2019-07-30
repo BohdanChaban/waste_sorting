b6d09a479b45eea26635cf02c47929d197674c4a
Chamelek <stetsyshyn.vo@gmail.com>
2019-07-30 13:56:25 +0300

    changes in register contoller devise

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
+  def create
+    if current_user
+      @user = User.new(sign_up_params)
+      @user.save ? (redirect_to root_path, notice: 'User succesfully created!') : (render :new)
+    else
+      super
+    end
+  end
+
   protected
+
+  before_action :authorize_user!, only: %i[new create]
+  skip_before_action :require_no_authentication, only: %i[new create]
+
+  def authorize_user!
+    redirect_to '/', notice: 'Your permissions do not allow access to this page' unless current_user.admin?
+  end
 
   def after_sign_up_path_for(_resource)
     new_account_path
