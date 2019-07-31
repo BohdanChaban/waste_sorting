f48b01aac15ba752aa6e599fb2092294e32ac130
Chamelek <stetsyshyn.vo@gmail.com>
2019-07-31 13:51:45 +0300

    add update managers by admin possibility

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,22 +9,36 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def edit
-    @user = User.find(params[:id])
+    if current_user.admin?
+      set_user
+      redirect_to root_path unless user_role_check
+    else
+      super
+    end
   end
 
-  # def update
-  #   @user = User.find(params[:id])
-  #   if @user.update_attributes(user_params)
-  #     redirect_to user_url, notice: 'Updated User.'
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    if current_user.admin?
+      user_role_check
+      @user.update_attributes(account_update_params) ? (redirect_to accounts_path(role: 'manager'), notice: 'Updated User.') : (render :edit)
+    else
+      super
+    end
+  end
 
   protected
 
   before_action :authorize_user!, only: %i[new create]
   skip_before_action :require_no_authentication, only: %i[new create]
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_role_check
+    set_user
+    @user if @user.role == 'manager'
+  end
 
   def authorize_user!
     redirect_to '/', notice: 'Your permissions do not allow access to this page' unless current_user.admin?
