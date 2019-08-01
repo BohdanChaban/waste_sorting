c03809c51a44be8a2b9a916ee8a9806cc7fc980c
Chamelek <stetsyshyn.vo@gmail.com>
2019-08-01 13:21:12 +0300

    fix missed indent | callbacks

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :authorize_user!, only: %i[new create]
+  skip_before_action :require_no_authentication, only: %i[new create]
+
   def create
     if current_user
       @user = User.new(sign_up_params)
@@ -32,9 +35,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   protected
-
-  before_action :authorize_user!, only: %i[new create]
-  skip_before_action :require_no_authentication, only: %i[new create]
 
   def set_user
     @user = User.find(params[:id])
